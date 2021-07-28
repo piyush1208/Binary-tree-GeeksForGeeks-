ad8eb7189e97d04aa84b4a4432b90b4f3f7f89e6
Piyush Sharma <56079444+piyush1208@users.noreply.github.com>
2021-07-28 17:02:58 +0530

    Add files via upload

---
+++ tree to sumTree.cpp
@@ -0,0 +1,15 @@
+int solve(Node *node){
+    if(!node)return 0;
+    int a=solve(node->left);
+    int b=solve(node->right);
+    int x=node->data;
+    node->data=a+b;
+    
+    return a+b+x;
+}
+void toSumTree(Node *node)
+{
+    
+    
+    solve(node);
+}
