8e292bc8469cfde1e9077a2d0539252b78e3f921
Piyush Sharma <56079444+piyush1208@users.noreply.github.com>
2021-07-28 17:10:34 +0530

    Add files via upload

---
+++ Print all k-sum paths in a binary tree.cpp
@@ -0,0 +1,26 @@
+void func(Node * root,vector<int> & path,int k){
+	if(!root)return;
+	
+	path.push_back(root->data);
+	
+	func(root->left,path,k);
+	func(root->right,path,k);
+	
+	int f=0;
+	
+	for(int j=path.size()-1;j>=0;j--){
+		f=f+path[j];
+		if(f==k){
+			for(int m=j;m<path.size();m++){
+				cout << path[m]<<" ";
+			}
+			cout << endl;
+		}
+	}
+	path.pop_back();	
+}
+
+int main(){
+	vector<int>path;
+	func(root,path,k);
+}
